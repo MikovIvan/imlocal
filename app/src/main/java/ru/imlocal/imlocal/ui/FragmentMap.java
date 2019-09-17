19dca5397e3888492daac5050050ee458dcf392c
Ivan <mikov_ivan@list.ru>
2019-09-17 18:30:23 +0300

    обработка тапа по маркеру

--- app/src/main/java/ru/imlocal/imlocal/ui/FragmentMap.java
+++ app/src/main/java/ru/imlocal/imlocal/ui/FragmentMap.java
@@ -14,7 +14,6 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.squareup.picasso.Picasso;
 import com.yandex.mapkit.geometry.Point;
@@ -24,6 +23,7 @@ import com.yandex.mapkit.map.CameraPosition;
 import com.yandex.mapkit.map.CameraUpdateSource;
 import com.yandex.mapkit.map.CompositeIcon;
 import com.yandex.mapkit.map.IconStyle;
+import com.yandex.mapkit.map.InputListener;
 import com.yandex.mapkit.map.Map;
 import com.yandex.mapkit.map.MapObject;
 import com.yandex.mapkit.map.MapObjectCollection;
@@ -38,7 +38,6 @@ import com.yandex.runtime.image.ImageProvider;
 
 import ru.imlocal.imlocal.MainActivity;
 import ru.imlocal.imlocal.R;
-import ru.imlocal.imlocal.adaptor.RecyclerViewAdapterShops;
 import ru.imlocal.imlocal.entity.Shop;
 import ru.imlocal.imlocal.utils.Utils;
 
@@ -48,10 +47,10 @@ import static ru.imlocal.imlocal.ui.FragmentListPlaces.shopList;
 import static ru.imlocal.imlocal.utils.Constants.BASE_IMAGE_URL;
 import static ru.imlocal.imlocal.utils.Constants.SHOP_IMAGE_DIRECTION;
 
-public class FragmentMap extends Fragment implements UserLocationObjectListener, CameraListener, RecyclerViewAdapterShops.OnItemClickListener {
+public class FragmentMap extends Fragment implements UserLocationObjectListener, CameraListener, InputListener {
 
-    private RecyclerView rvPlaces;
-    private RecyclerViewAdapterShops adapter;
+    private boolean isSelected;
+    private PlacemarkMapObject selected;
 
     private MapView mapView;
     private UserLocationLayer userLocationLayer;
@@ -70,10 +69,19 @@ public class FragmentMap extends Fragment implements UserLocationObjectListener,
         @Override
         public boolean onMapObjectTap(MapObject mapObject, Point point) {
             cardView.setVisibility(View.VISIBLE);
+
             if (mapObject instanceof PlacemarkMapObject) {
                 PlacemarkMapObject shopObject = (PlacemarkMapObject) mapObject;
-                shopObject.setIconStyle(new IconStyle().setScale(1.5f));
                 Object userData = shopObject.getUserData();
+                if (isSelected) {
+                    isSelected = false;
+                    selected.setIconStyle(new IconStyle().setScale(0.5f));
+                }
+                isSelected = true;
+                selected = shopObject;
+                shopObject.setIconStyle(new IconStyle().setScale(1.5f).setAnchor(new PointF(0.5f, 0.52f))
+                        .setRotationType(RotationType.ROTATE)
+                        .setZIndex(0f));
                 if (userData instanceof Shop) {
                     shop = (Shop) userData;
                     tvShopTitle.setText(shop.getShopShortName());
@@ -124,6 +132,7 @@ public class FragmentMap extends Fragment implements UserLocationObjectListener,
 
         mapView = view.findViewById(R.id.mapview);
         mapView.getMap().setRotateGesturesEnabled(false);
+        mapView.getMap().addInputListener(this);
 
 //        mapView.getMap().move(new CameraPosition(new Point(latitude, longitude), 14, 0, 0));
 //        для теста
@@ -144,7 +153,8 @@ public class FragmentMap extends Fragment implements UserLocationObjectListener,
     private void createMapObjects() {
         for (Shop shop : shopList) {
             PlacemarkMapObject placemarkMapObject = mapObjects.addPlacemark(new Point(shop.getShopAddress().getLatitude(), shop.getShopAddress().getLongitude()),
-                    ImageProvider.fromResource(getActivity(), R.drawable.ic_place));
+                    ImageProvider.fromResource(getActivity(), R.drawable.untitled));
+            placemarkMapObject.setIconStyle(new IconStyle().setScale(0.5f).setAnchor(new PointF(0.5f, 0.5f)));
             placemarkMapObject.setUserData(shop);
             placemarkMapObject.addTapListener(mapObjectTapListener);
         }
@@ -212,7 +222,15 @@ public class FragmentMap extends Fragment implements UserLocationObjectListener,
     }
 
     @Override
-    public void onItemClick(int position) {
+    public void onMapTap(@NonNull Map map, @NonNull Point point) {
+        if (selected != null) {
+            isSelected = false;
+            selected.setIconStyle(new IconStyle().setScale(0.5f));
+        }
+    }
+
+    @Override
+    public void onMapLongTap(@NonNull Map map, @NonNull Point point) {
 
     }
 }
