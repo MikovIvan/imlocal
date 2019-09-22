247fdcc81958c7878beb5665d6a07b61cba3f1dd
Ivan <mikov_ivan@list.ru>
2019-09-22 15:34:48 +0300

    добавлены кнопки на карту и сохранение выбранного магазина

--- app/src/main/java/ru/imlocal/imlocal/ui/FragmentMap.java
+++ app/src/main/java/ru/imlocal/imlocal/ui/FragmentMap.java
@@ -6,8 +6,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,6 +19,7 @@ import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import com.squareup.picasso.Picasso;
+import com.yandex.mapkit.Animation;
 import com.yandex.mapkit.geometry.Point;
 import com.yandex.mapkit.layers.ObjectEvent;
 import com.yandex.mapkit.map.CameraListener;
@@ -39,6 +43,7 @@ import com.yandex.runtime.image.ImageProvider;
 import ru.imlocal.imlocal.MainActivity;
 import ru.imlocal.imlocal.R;
 import ru.imlocal.imlocal.entity.Shop;
+import ru.imlocal.imlocal.utils.PreferenceUtils;
 import ru.imlocal.imlocal.utils.Utils;
 
 import static ru.imlocal.imlocal.MainActivity.latitude;
@@ -47,7 +52,9 @@ import static ru.imlocal.imlocal.ui.FragmentListPlaces.shopList;
 import static ru.imlocal.imlocal.utils.Constants.BASE_IMAGE_URL;
 import static ru.imlocal.imlocal.utils.Constants.SHOP_IMAGE_DIRECTION;
 
-public class FragmentMap extends Fragment implements UserLocationObjectListener, CameraListener, InputListener {
+public class FragmentMap extends Fragment implements UserLocationObjectListener, CameraListener, InputListener, View.OnClickListener {
+    private static final float USER_ZOOM_DURATION = 2.0f;
+    private static final float ZOOM_DURATION = 0.3f;
 
     private boolean isSelected;
     private PlacemarkMapObject selected;
@@ -65,6 +72,21 @@ public class FragmentMap extends Fragment implements UserLocationObjectListener,
     private TextView tvShopDescription;
     private TextView tvShopRating;
     private CardView cardView;
+
+    private ImageButton ibPlus;
+    private ImageButton ibMinus;
+    private ImageButton ibUserLocation;
+    private Button btnFilter;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+        ((MainActivity) getActivity()).enableUpButtonViews(true);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setBackgroundDrawable(getContext().getResources().getDrawable(R.drawable.toolbar_transparent));
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setIcon(new ColorDrawable(getResources().getColor(android.R.color.transparent)));
+    }
+
     private MapObjectTapListener mapObjectTapListener = new MapObjectTapListener() {
         @Override
         public boolean onMapObjectTap(MapObject mapObject, Point point) {
@@ -79,36 +101,19 @@ public class FragmentMap extends Fragment implements UserLocationObjectListener,
                 }
                 isSelected = true;
                 selected = shopObject;
-                shopObject.setIconStyle(new IconStyle().setScale(1.5f).setAnchor(new PointF(0.5f, 0.85f))
-                        .setRotationType(RotationType.ROTATE)
-                        .setZIndex(0f));
+                shopObject.setIconStyle(new IconStyle().setScale(1.5f).setAnchor(new PointF(0.5f, 0.85f)));
                 if (userData instanceof Shop) {
                     shop = (Shop) userData;
-                    tvShopTitle.setText(shop.getShopShortName());
-                    Picasso.with(getActivity()).load(BASE_IMAGE_URL + SHOP_IMAGE_DIRECTION + shop.getShopPhotoArray().get(0).getShopPhoto())
-                            .into(ivShopIcon);
-                    tvShopDescription.setText(shop.getShopShortDescription());
-                    tvShopRating.setText(String.valueOf(shop.getShopAvgRating()));
+                    setDataToView(shopObject, shop);
 
 //                    Log.d("DIS", latitude + " " + longitude);
 //                    Log.d("DIS", String.valueOf(shopObject.getGeometry().getLatitude() +" "+ shopObject.getGeometry().getLongitude()));
 //                    Log.d("DIS", String.valueOf(Geo.distance(shopObject.getGeometry(), new Point(latitude, longitude))));
-
-                    tvDistance.setText(Utils.getDistance(shopObject, latitude, longitude));
                 }
             }
             return true;
         }
     };
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
-        ((MainActivity) getActivity()).enableUpButtonViews(true);
-        ((AppCompatActivity) getActivity()).getSupportActionBar().setBackgroundDrawable(getContext().getResources().getDrawable(R.drawable.toolbar_transparent));
-        ((AppCompatActivity) getActivity()).getSupportActionBar().setIcon(new ColorDrawable(getResources().getColor(android.R.color.transparent)));
-    }
 
     @Nullable
     @Override
@@ -121,14 +126,17 @@ public class FragmentMap extends Fragment implements UserLocationObjectListener,
         tvShopTitle = view.findViewById(R.id.tv_title);
         tvShopDescription = view.findViewById(R.id.tv_description);
         tvShopRating = view.findViewById(R.id.tv_rating);
-        cardView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Bundle bundle = new Bundle();
-                bundle.putSerializable("shop", shop);
-                ((MainActivity) getActivity()).openVitrinaShop(bundle);
-            }
-        });
+
+        ibPlus = view.findViewById(R.id.ib_plus);
+        ibMinus = view.findViewById(R.id.ib_minus);
+        ibUserLocation = view.findViewById(R.id.ib_user_location);
+        btnFilter = view.findViewById(R.id.btn_filter);
+
+        cardView.setOnClickListener(this);
+        ibPlus.setOnClickListener(this);
+        ibMinus.setOnClickListener(this);
+        ibUserLocation.setOnClickListener(this);
+        btnFilter.setOnClickListener(this);
 
         mapView = view.findViewById(R.id.mapview);
         mapView.getMap().setRotateGesturesEnabled(false);
@@ -145,18 +153,31 @@ public class FragmentMap extends Fragment implements UserLocationObjectListener,
 //        userLocationLayer.setVisible(true);
 //        userLocationLayer.setHeadingEnabled(true);
 //        userLocationLayer.setObjectListener(this);
-
         createMapObjects();
+
         return view;
+    }
+
+    private void setDataToView(PlacemarkMapObject shopObject, Shop shop) {
+        tvShopTitle.setText(shop.getShopShortName());
+        Picasso.with(getActivity()).load(BASE_IMAGE_URL + SHOP_IMAGE_DIRECTION + shop.getShopPhotoArray().get(0).getShopPhoto())
+                .into(ivShopIcon);
+        tvShopDescription.setText(shop.getShopShortDescription());
+        tvShopRating.setText(String.valueOf(shop.getShopAvgRating()));
+        tvDistance.setText(Utils.getDistance(shopObject, latitude, longitude));
     }
 
     private void createMapObjects() {
         for (Shop shop : shopList) {
-            PlacemarkMapObject placemarkMapObject = mapObjects.addPlacemark(new Point(shop.getShopAddress().getLatitude(), shop.getShopAddress().getLongitude()),
-                    ImageProvider.fromResource(getActivity(), R.drawable.ic_marker));
-            placemarkMapObject.setIconStyle(new IconStyle().setScale(0.5f).setAnchor(new PointF(0.5f, 0.5f)));
-            placemarkMapObject.setUserData(shop);
-            placemarkMapObject.addTapListener(mapObjectTapListener);
+            if (PreferenceUtils.getShop(getActivity()) != null) {
+                if (shop.getShopId() == (PreferenceUtils.getShop(getActivity()).getShopId())) {
+                    addSelectedPlaceMark(shop, 1.5f, 0.5f, 0.85f);
+                } else {
+                    addPlaceMark(shop, 0.5f, 0.5f, 0.5f);
+                }
+            } else {
+                addPlaceMark(shop, 0.5f, 0.5f, 0.5f);
+            }
         }
     }
 
@@ -205,8 +226,7 @@ public class FragmentMap extends Fragment implements UserLocationObjectListener,
 
     @Override
     public void onCameraPositionChanged(@NonNull Map map, @NonNull CameraPosition cameraPosition, @NonNull CameraUpdateSource cameraUpdateSource, boolean finish) {
-//        rvPlaces.setVisibility(View.INVISIBLE);
-        cardView.setVisibility(View.INVISIBLE);
+        cardView.setVisibility(View.GONE);
 //        if (finish) {
 //            if (followUserLocation) {
 //                userLocationLayer.setAnchor(
@@ -214,9 +234,7 @@ public class FragmentMap extends Fragment implements UserLocationObjectListener,
 //                        new PointF((float) (mapView.getWidth() * 0.5), (float) (mapView.getHeight() * 0.83)));
 //                followUserLocation = false;
 //            } else {
-//                if (!followUserLocation) {
-//                    userLocationLayer.resetAnchor();
-//                }
+//                userLocationLayer.resetAnchor();
 //            }
 //        }
     }
@@ -227,10 +245,67 @@ public class FragmentMap extends Fragment implements UserLocationObjectListener,
             isSelected = false;
             selected.setIconStyle(new IconStyle().setScale(0.5f));
         }
+        cardView.setVisibility(View.GONE);
     }
 
     @Override
     public void onMapLongTap(@NonNull Map map, @NonNull Point point) {
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ib_plus:
+                mapView.getMap().move(
+                        new CameraPosition(mapView.getMap().getCameraPosition().getTarget(), mapView.getMap().getCameraPosition().getZoom() + 1f, 0.0f, 0.0f),
+                        new Animation(Animation.Type.SMOOTH, ZOOM_DURATION),
+                        null);
+                break;
+            case R.id.ib_minus:
+                mapView.getMap().move(
+                        new CameraPosition(mapView.getMap().getCameraPosition().getTarget(), mapView.getMap().getCameraPosition().getZoom() - 1f, 0.0f, 0.0f),
+                        new Animation(Animation.Type.SMOOTH, ZOOM_DURATION),
+                        null);
+                break;
+            case R.id.ib_user_location:
+                Toast.makeText(getActivity(), "Ждем апи", Toast.LENGTH_LONG).show();
+//                if (userLocationLayer.cameraPosition() != null) {
+//                    mapView.getMap().move(
+//                            new CameraPosition(userLocationLayer.cameraPosition().getTarget(), 14, 0.0f, 0.0f),
+//                            new Animation(Animation.Type.SMOOTH, USER_ZOOM_DURATION),
+//                            null);
+//                }
+                break;
+            case R.id.cardview_map:
+                PreferenceUtils.saveShop(shop, getActivity());
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("shop", shop);
+                ((MainActivity) getActivity()).openVitrinaShop(bundle);
+                break;
+            case R.id.btn_filter:
+                Toast.makeText(getActivity(), "Что сюда вставить?", Toast.LENGTH_LONG).show();
+                break;
+        }
+    }
+
+    private void addPlaceMark(Shop shop, float scale, float x, float y) {
+        PlacemarkMapObject placemarkMapObject = mapObjects.addPlacemark(new Point(shop.getShopAddress().getLatitude(), shop.getShopAddress().getLongitude()),
+                ImageProvider.fromResource(getActivity(), R.drawable.ic_marker));
+        placemarkMapObject.setIconStyle(new IconStyle().setScale(scale).setAnchor(new PointF(x, y)));
+        placemarkMapObject.setUserData(shop);
+        placemarkMapObject.addTapListener(mapObjectTapListener);
+    }
+
+    private void addSelectedPlaceMark(Shop shop, float scale, float x, float y) {
+        addPlaceMark(shop, 1.5f, 0.5f, 0.85f);
+        PlacemarkMapObject placemarkMapObject = mapObjects.addPlacemark(new Point(shop.getShopAddress().getLatitude(), shop.getShopAddress().getLongitude()),
+                ImageProvider.fromResource(getActivity(), R.drawable.ic_marker));
+        placemarkMapObject.setIconStyle(new IconStyle().setScale(1.5f).setAnchor(new PointF(0.5f, 0.85f)));
+        placemarkMapObject.setUserData(shop);
+        placemarkMapObject.addTapListener(mapObjectTapListener);
+        selected = placemarkMapObject;
+        setDataToView(placemarkMapObject, shop);
+        cardView.setVisibility(View.VISIBLE);
     }
 }
