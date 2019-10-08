7521a7cf9f9cb5d1a7ff1cc7bc684746b5f6eb07
Ivan <mikov_ivan@list.ru>
2019-10-08 22:19:29 +0300

    адаптер для расписания

---
+++ app/src/main/java/ru/imlocal/imlocal/adaptor/RecyclerViewAdapterWeekDays.java
@@ -0,0 +1,105 @@
+package ru.imlocal.imlocal.adaptor;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ru.imlocal.imlocal.R;
+import ru.imlocal.imlocal.entity.Category;
+
+public class RecyclerViewAdapterWeekDays extends RecyclerView.Adapter<RecyclerViewAdapterWeekDays.ViewHolder> {
+    private int category_index;
+    private List<Category> data;
+    private Context context;
+    private RecyclerViewAdapterWeekDays.OnItemCategoryClickListener mListener;
+
+    public RecyclerViewAdapterWeekDays(Context context, String category) {
+        switch (category) {
+            case "week_days":
+                category_index = -1;
+                data = new ArrayList<>();
+                data.add(new Category("Пн"));
+                data.add(new Category("Вт"));
+                data.add(new Category("Ср"));
+                data.add(new Category("Чт"));
+                data.add(new Category("Пт"));
+                data.add(new Category("Сб"));
+                data.add(new Category("Вс"));
+                this.context = context;
+                break;
+        }
+
+    }
+
+    public void setOnItemClickListener(RecyclerViewAdapterWeekDays.OnItemCategoryClickListener listener) {
+        mListener = listener;
+    }
+
+    @Override
+    public RecyclerViewAdapterWeekDays.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_category, parent, false);
+
+        return new RecyclerViewAdapterWeekDays.ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerViewAdapterWeekDays.ViewHolder holder, final int position) {
+        Category category = data.get(position);
+        holder.tvCategory.setText(category.getName());
+
+        holder.tvCategory.setBackgroundColor(category.isSelected() ? context.getResources().getColor(R.color.color_main) : Color.WHITE);
+        holder.tvCategory.setTextColor(category.isSelected() ? Color.WHITE : context.getResources().getColor(R.color.color_main));
+        holder.tvCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mListener != null) {
+                    if (position != RecyclerView.NO_POSITION) {
+                        mListener.onItemClickCategory(position);
+                    }
+                }
+                category.setSelected(!category.isSelected());
+                holder.tvCategory.setBackgroundColor(category.isSelected() ? context.getResources().getColor(R.color.color_main) : Color.WHITE);
+                holder.tvCategory.setTextColor(category.isSelected() ? Color.WHITE : context.getResources().getColor(R.color.color_main));
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public interface OnItemCategoryClickListener {
+        void onItemClickCategory(int position);
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvCategory;
+
+        ViewHolder(View v) {
+            super(v);
+            tvCategory = v.findViewById(R.id.btn_category);
+
+            v.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mListener != null) {
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION) {
+                            mListener.onItemClickCategory(position);
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ app/src/main/java/ru/imlocal/imlocal/entity/Category.java
@@ -0,0 +1,26 @@
+package ru.imlocal.imlocal.entity;
+
+public class Category {
+    private String name;
+    private boolean isSelected;
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isSelected() {
+        return isSelected;
+    }
+
+    public void setSelected(boolean selected) {
+        isSelected = selected;
+    }
+}
