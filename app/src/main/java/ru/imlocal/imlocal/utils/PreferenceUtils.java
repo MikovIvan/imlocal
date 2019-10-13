c2c217f50d25a8d04eafc09194ae1dfee3a64669
Ivan <mikov_ivan@list.ru>
2019-10-13 16:05:23 +0300

    сохранение события/акции

--- app/src/main/java/ru/imlocal/imlocal/utils/PreferenceUtils.java
+++ app/src/main/java/ru/imlocal/imlocal/utils/PreferenceUtils.java
@@ -6,9 +6,18 @@ import android.preference.PreferenceManager;
 
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import ru.imlocal.imlocal.entity.Action;
+import ru.imlocal.imlocal.entity.Event;
 import ru.imlocal.imlocal.entity.Shop;
 import ru.imlocal.imlocal.entity.User;
 
+import static ru.imlocal.imlocal.utils.Constants.ACTION;
+import static ru.imlocal.imlocal.utils.Constants.EVENT;
 import static ru.imlocal.imlocal.utils.Constants.REQUESTING_LOCATION_PERMISSON;
 import static ru.imlocal.imlocal.utils.Constants.SHOP;
 import static ru.imlocal.imlocal.utils.Constants.TAB;
@@ -73,5 +82,55 @@ public class PreferenceUtils {
         Gson gson = new Gson();
         String json = prefs.getString(SHOP, "");
         return gson.fromJson(json, Shop.class);
+    }
+
+    public static void saveEvent(Event event, Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor prefsEditor = prefs.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(event);
+        prefsEditor.putString(EVENT, json);
+        prefsEditor.apply();
+    }
+
+    public static Event getEvent(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        Gson gson = new Gson();
+        String json = prefs.getString(EVENT, "");
+        return gson.fromJson(json, Event.class);
+    }
+
+    public static void saveAction(Action action, Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor prefsEditor = prefs.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(action);
+        prefsEditor.putString(ACTION, json);
+        prefsEditor.apply();
+    }
+
+    public static Action getAction(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        Gson gson = new Gson();
+        String json = prefs.getString(ACTION, "");
+        return gson.fromJson(json, Action.class);
+    }
+
+    public static void savePhotoPathList(List<String> photosPathList, Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor prefsEditor = prefs.edit();
+        Set<String> set = new HashSet<>(photosPathList);
+        prefsEditor.putStringSet("photosPathList", set);
+        prefsEditor.apply();
+    }
+
+    public static List<String> getPhotoPathList(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        Set<String> set = prefs.getStringSet("photosPathList", null);
+        List<String> photosPathList = new ArrayList<>();
+        if(set!=null){
+            photosPathList.addAll(set);
+        }
+        return photosPathList;
     }
 }
